0168ef104ccee22ea22efd108c2ba88f46b75c66
Serhan Baymaz <serhanbaymaz1@gmail.com>
2025-12-31 03:16:23 +0300

    Add health check implementation and tests for API endpoints

--- API/Extensions/ApiExtensions.cs
+++ API/Extensions/ApiExtensions.cs
@@ -2,6 +2,7 @@ using API.Helpers;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace API.Extensions;
 
@@ -26,19 +27,20 @@ public static class ApiExtensions
 
     public static void AddHealthCheckServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var healthChecks = services.AddHealthChecks();
+        var healthChecks = services.AddHealthChecks()
+            .AddCheck("API", () => HealthCheckResult.Healthy(), tags: new[] { "api" });
 
         var connectionString = configuration["ConnectionStrings:DefaultConnection"];
         if (!string.IsNullOrEmpty(connectionString))
         {
-            healthChecks.AddNpgSql(connectionString, name: "PostgreSQL", tags: new[] { "dependency" });
+            healthChecks.AddNpgSql(connectionString, name: "PostgreSQL", tags: new[] { "dependency", "postgresql" });
         }
 
         var seqUrl = configuration["Serilog:WriteTo:1:Args:serverUrl"];
         if (!string.IsNullOrEmpty(seqUrl) && Uri.TryCreate(seqUrl, UriKind.Absolute, out var uri))
         {
             // Check if Seq ingestion port is open (TCP) because HTTP root might return 404
-            healthChecks.AddTcpHealthCheck(setup => setup.AddHost(uri.Host, uri.Port), name: "Seq", tags: new[] { "dependency" });
+            healthChecks.AddTcpHealthCheck(setup => setup.AddHost(uri.Host, uri.Port), name: "Seq", tags: new[] { "dependency", "seq" });
         }
     }
 
